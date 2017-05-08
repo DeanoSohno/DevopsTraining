6e86c824acd06a9958f3cf7b0e25f25418dc002d
Dean Sohn <dean.sohn@capgemini.com>
2017-05-08 14:03:37 +0100

    Added new imports

--- src/main/java/devops/training/DeandemoApplication.java
+++ src/main/java/devops/training/DeandemoApplication.java
@@ -1,12 +1,13 @@
 package devops.training;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
 
 @SpringBootApplication
+@RestController
 public class DeandemoApplication {
 
 	@GetMapping("/")
