c3d81cb8352b054c2f66dd93b05504794597ef8c
Dean Sohn <deansohn@Deans-MacBook-Pro.local>
2017-05-08 10:03:44 +0100

    Created Spring Boot project deandemo

---
+++ src/main/java/devops/training/DeandemoApplication.java
@@ -0,0 +1,12 @@
+package devops.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeandemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeandemoApplication.class, args);
+	}
+}
---
+++ src/test/java/devops/training/DeandemoApplicationTests.java
@@ -0,0 +1,16 @@
+package devops.training;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeandemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
