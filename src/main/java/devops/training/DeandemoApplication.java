270357f81c94b04cf2c8a1a73f2ab8bbc9830f4b
Dean Sohn <dean.sohn@capgemini.com>
2017-05-08 11:25:01 +0100

    added commandlinerunner code

--- src/main/java/devops/training/DeandemoApplication.java
+++ src/main/java/devops/training/DeandemoApplication.java
@@ -1,12 +1,24 @@
 package devops.training;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeandemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeandemoApplication.class, args);
+	@Bean
+	public CommandLineRunner cmdRunner() {
+		return new	CommandLineRunner() {
+			@Override
+			public void run(String... strings) throws Exception {
+				System.out.println("DemoApplication.run");
+			}
+		};
 	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeandemoApplication.class, args);
+    }
 }
